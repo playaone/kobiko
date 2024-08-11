2e22eb96a7f73cb409c31fecca579929901d9e94
Nwankwo Christopher <dev.christopher007@gmail.com>
2024-08-11 21:19:02 +0100

    update run.py

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 import os
 from app import create_app
 
-host = os.environ['APP_HOST']
-port = os.environ['APP_PORT']
-
 app = create_app()
 
 # 54.191.253.12
 # 44.226.122.3
 
 if __name__ == '__main__':
-    app.run(debug=True, host=host, port=port)
+    app.run()
